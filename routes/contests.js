030cc0dfb0f18dfaf23837f497a76f6589e73984
GDmail <GDmail91@gmail.com>
2016-02-12 07:16:18 +0900

    contests IO process done

--- routes/contests.js
+++ routes/contests.js
@@ -179,6 +179,105 @@ router.post('/', function(req, res) {
     }
 });
 
+/* GET applies list */
+router.get('/applications', function(req, res, next) {
+    if (!req.query.access_token) {
+        return res.send({
+            result: false,
+            msg: "로그인이 필요합니다."
+        });
+    } else {
+        var data = {
+            'access_token': req.query.access_token,
+        };
+
+        // 신청자 목록 가져오는 프로세스
+        var async = require('async');
+        async.waterfall([
+                function(callback) {
+                    // 사용자 인증
+                    pool.getConnection(function (err, connection) {
+                        if (err) callback(err);
+                        var select = [data.access_token];
+                        connection.query("SELECT users_id FROM Users WHERE kakao_access_token = ?", select, function (err, rows) {
+                            if (err) {
+                                connection.release();
+                                return res.send({result: false, msg: "사용자 정보를 가져오는데 실패했습니다. 원인: " + err});
+                            }
+                            connection.release();
+
+                            if (rows.length != 0) {
+                                callback(null, { users_id : rows[0].users_id });
+                            } else {
+                                callback({result: false, msg: '잘못된 접근입니다.'});
+                            }
+                        });
+                    });
+                },
+                function(back_data, callback) {
+                    // 신청서 정보 확인
+                    pool.getConnection(function (err, connection) {
+                        if (err) callback(err);
+                        var select = ['Applies', back_data.users_id];
+                        connection.query("SELECT * FROM ?? WHERE app_users_id = ?", select, function (err, rows) {
+                            if (err) {
+                                connection.release();
+                                return res.send({result: false, msg: "신청서 정보를 가져오는데 실패했습니다. 원인: " + err});
+                            }
+
+                            if (rows.length != 0) {
+                                callback(null, rows);
+                            } else {
+                                callback({result: false, msg: '신청한 공고가 없습니다.'});
+                            }
+                        });
+                    });
+                },
+                function(back_data, callback) {
+                    // 모집글 정보 가져옴
+                    pool.getConnection(function (err, connection) {
+                        // TODO members, appliers, clips, 가져오는 것 (JOIN 해야함)
+                        // TODO members 는 Applies테이블 중 is_check가 트루인 사람들, applier는 나머지 전부, clips 는 Clips 테이블중 contests_id를 가지고 있는것등
+                        var sql = "SELECT contests_id, title, recruitment, cont_writer, hosts, categories, period, cover, positions, views FROM Contests WHERE contests_id IN (";
+
+                        // contests id 갯수만큼 where절에 추가하기
+                        var length = 0;
+                        back_data.forEach(function (val, index, arr) {
+                            if(length == 0) sql += val.app_contests_id;
+                            sql += "," + val.app_contests_id;
+                            length ++;
+                            if (length == back_data.length) {
+                                sql += ")";
+
+                                console.log(sql);
+                                connection.query(sql, function (err, rows) {
+                                    if (err) {
+                                        connection.release();
+                                        return callback({ result: false, msg: "모집글 정보를 가져오는데 실패했습니다. 원인: "+err });
+                                    }
+                                    connection.release();
+
+                                    callback(null, rows);
+                                });
+                            }
+                        });
+                    });
+                }
+            ],
+            function(err, result) {
+                // 취소 결과 출력
+                if (err) return res.send(err);
+                var dummy_data = {
+                    result: true,
+                    msg: "신청목록 가져옴",
+                    data: result
+                };
+                res.statusCode = 200;
+                res.send(dummy_data);
+            });
+    }
+});
+
 /* GET contests detail view */
 router.get('/:contest_id', function(req, res) {
     var data = {
@@ -748,135 +847,92 @@ router.post('/:contest_id/:applies_id', function(req, res) {
 
 /* DELETE cancel apply */
 router.delete('/:contest_id/:applies_id', function(req, res, next) {
-    if (!req.session.is_login) {
+    if (!req.body.access_token) {
         return res.send({
             result: false,
             msg: "로그인이 필요합니다."
         });
+    } else {
+        var data = {
+            'access_token': req.body.access_token,
+            'contest_id': req.params.contest_id,
+            'applies_id': req.params.applies_id
+        };
+
+        // 신청서 삭제 프로세스
+        var async = require('async');
+        async.waterfall([
+                function(callback) {
+                    // 사용자 인증
+                    pool.getConnection(function (err, connection) {
+                        if (err) callback(err);
+                        var select = [data.access_token];
+                        connection.query("SELECT users_id FROM Users WHERE kakao_access_token = ?", select, function (err, rows) {
+                            if (err) {
+                                connection.release();
+                                return callback({result: false, msg: "사용자 정보를 가져오는데 실패했습니다. 원인: " + err});
+                            }
+                            connection.release();
+
+                            if (rows.length != 0) {
+                                callback(null, { users_id : rows[0].users_id });
+                            } else {
+                                callback({result: false, msg: '잘못된 접근입니다.'});
+                            }
+                        });
+                    });
+                },
+                function(back_data, callback) {
+                    // 신청서 정보 확인
+                    pool.getConnection(function (err, connection) {
+                        if (err) callback(err);
+                        var select = ['Applies', data.applies_id, data.contest_id, back_data.users_id];
+                        connection.query("SELECT * FROM ?? WHERE applies_id = ? AND app_contests_id = ? AND app_users_id = ?", select, function (err, rows) {
+                            if (err) {
+                                connection.release();
+                                return callback({result: false, msg: "신청서 정보를 가져오는데 실패했습니다. 원인: " + err});
+                            }
+
+                            if (rows.length != 0) {
+                                callback(null, back_data);
+                            } else {
+                                callback({result: false, msg: '신청한 공고가 없습니다.'});
+                            }
+                        });
+                    });
+                },
+                function(back_data, callback) {
+                    // DB에서 신청서 삭제
+                    pool.getConnection(function (err, connection) {
+                        if (err) {
+                            connection.release();
+                            return callback({result: false, msg: '처리중 오류가 발생했습니다. 원인: ' + err});
+                        }
+
+                        var select = ['Applies', data.applies_id];
+                        connection.query("DELETE FROM ?? WHERE applies_id = ?", select, function (err) {
+                            if (err) {
+                                connection.release();
+                                return callback({result: false, msg: '처리중 오류가 발생했습니다. 원인: ' + err});
+                            }
+                            connection.release();
+                            callback(null);
+                        });
+                    });
+                }
+            ],
+            function(err) {
+                // 취소 결과 출력
+                if (err) return res.send(err);
+                var dummy_data = {
+                    result: true,
+                    msg: "취소 되었습니다."
+                };
+                res.statusCode = 200;
+                res.send(dummy_data);
+            });
     }
-    var data = {
-        'user_id': req.session.userinfo.user_id,
-        'contest_id': req.params.contest_id,
-        'applies_id': req.params.applies_id
-    };
-
-    // TODO 신청서 삭제 프로세스
-
-    var dummy_data = {
-        result : true,
-        msg : "취소 되었습니다."
-    };
-    res.statusCode = 200;
-    res.send(dummy_data);
-
 });
 
-/* GET applies list */
-router.get('/applications', function(req, res, next) {
-    if (req.session.is_login) {
-        return res.send({
-            result: false,
-            msg: "로그인이 필요합니다."
-        });
-    }
-    var data = {
-        'user_id': req.session.userinfo.user_id,
-        'start_id': req.query.start_id,
-        'amount': req.query.amount
-    };
-
-    // TODO 신청자 목록 가져오는 프로세스
-
-    var dummy_data = {
-        result : true,
-        msg : "신청목록 가져옴",
-        data : [{
-            contests_id : 25,
-            title : "넥스터즈 공모전",
-            recruitment : 5,
-            writer : "홍길동",
-            hosts : "네이버",
-            categories : "개발/디지인",
-            period : "2016/07/24",
-            cover : "200자 소개글입니다.",
-            positions : "개발자/디자인/기획자",
-            members : 3,
-            aplliers : 20,
-            clips : 30,
-            views : 50
-        }, {
-            contests_id : 26,
-            title : "넥스터즈 공모전",
-            recruitment : 5,
-            writer : "홍길동",
-            hosts : "네이버",
-            categories : "개발/디지인",
-            period : "2016/07/24",
-            cover : "200자 소개글입니다.",
-            positions : "개발자/디자인/기획자",
-            members : 3,
-            aplliers : 20,
-            clips : 30,
-            views : 50
-        }, {
-            contests_id : 27,
-            title : "넥스터즈 공모전",
-            recruitment : 5,
-            writer : "홍길동",
-            hosts : "네이버",
-            categories : "개발/디지인",
-            period : "2016/07/24",
-            cover : "200자 소개글입니다.",
-            positions : "개발자/디자인/기획자",
-            members : 3,
-            aplliers : 20,
-            clips : 30,
-            views : 50
-        }, {
-            contests_id : 28,
-            title : "넥스터즈 공모전",
-            recruitment : 5,
-            writer : "홍길동",
-            hosts : "네이버",
-            categories : "개발/디지인",
-            period : "2016/07/24",
-            cover : "200자 소개글입니다.",
-            positions : "개발자/디자인/기획자",
-            members : 3,
-            aplliers : 20,
-            clips : 30,
-            views : 50
-        }, {
-            contests_id : 29,
-            title : "넥스터즈 공모전",
-            recruitment : 5,
-            writer : "홍길동",
-            hosts : "네이버",
-            categories : "개발/디지인",
-            period : "2016/07/24",
-            cover : "200자 소개글입니다.",
-            positions : "개발자/디자인/기획자",
-            members : 3,
-            aplliers : 20,
-            clips : 30,
-            views : 50
-        }, {
-            contests_id : 30,
-            title : "넥스터즈 공모전",
-            recruitment : 5,
-            writer : "홍길동",
-            hosts : "네이버",
-            categories : "개발/디지인",
-            period : "2016/07/24",
-            cover : "200자 소개글입니다.",
-            positions : "개발자/디자인/기획자",
-            members : 3,
-            aplliers : 20,
-            clips : 30,
-            views : 50
-        }]
-    }
-
-});
 
 module.exports = router;
