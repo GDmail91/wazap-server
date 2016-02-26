167430d09471a1620144f9ab98bb93531ea2bbbe
GDmail <GDmail91@gmail.com>
2016-02-26 22:18:10 +0900

    add msg when edit user info

--- models/users_model.js
+++ models/users_model.js
@@ -6,7 +6,7 @@ var pool = mysql.createPool({
     user : credentials.mysql.user,
     password : credentials.mysql.password,
     database: credentials.mysql.database,
-    connectionLimit: 20,
+    connectionLimit: 21,
     waitForConnections: false
 });
 
@@ -22,17 +22,18 @@ var users_model = {
             if (err) return callback({ result: false, msg: "에러 발생. 원인: "+err });
             var insert = [data.kakao_id, data.username, data.school, data.age, data.major, data.locate, data.introduce, data.exp, data.access_token];
             connection.query('UPDATE Users SET ' +
-                'kakao_id = ?, ' +
-                'username = ?, ' +
-                'school = ?, ' +
-                'age = ?, ' +
-                'major = ?, ' +
-                'locate = ?, ' +
-                'introduce = ?, ' +
-                'exp = ? WHERE facebook_access_token = ?', insert, function (err) {
+                '`kakao_id` = ?, ' +
+                '`username` = ?, ' +
+                '`school` = ?, ' +
+                '`age` =?, ' +
+                '`major` = ?, ' +
+                '`locate` = ?, ' +
+                '`introduce` = ?, ' +
+                '`exp` = ? WHERE facebook_access_token= ?', insert, function (err, rows) {
                 if (err) {
-                    connection.release();
                     return callback({ result: false, msg: "정보 수정에 실패했습니다. 원인: "+err });
+                } else if(rows.affectedRows == 0) {
+                    return callback({ result: false, msg: "정보 수정에 실패했습니다. 원인: 적용되지않음" });
                 }
                 connection.release();
 
